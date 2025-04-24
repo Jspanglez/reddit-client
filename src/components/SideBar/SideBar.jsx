90848a745d2ea8c1b059817aa373694502aa63e7
Jeev Pangli <jspanglez@gmail.com>
2025-04-24 18:15:49 +0100

    Add an array of buttons that make up the Side Bar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import styles from './SideBar.module.css'
+
 const subs = []
 
 let i = 1
@@ -8,3 +11,16 @@ while (i <= 10) {
   subs.push(newSub)
   i++
 }
+
+function SideBar() {
+  return (
+    <div className={styles.subContainer}>
+      <h2>Subreddits</h2>
+      {subs.map(x => (
+        <button key={x.key} className={styles.subreddit}>{x.name}</button>
+      ))}
+    </div>
+  )
+}
+
+export default SideBar
