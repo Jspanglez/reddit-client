4c730096fc816d5075ff525924dcf0c0d8f81f01
Jeev Pangli <jspanglez@gmail.com>
2025-04-24 18:16:12 +0100

    Add the SideBar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 import Vote from './components/Vote/Vote'
 import Header from './components/Header/Header'
+import SideBar from './components/SideBar/SideBar'
 
 function App() {
   return (
     <>
       <Header />
+      <SideBar />
       {/* <Vote/> */}
     </>
   )
