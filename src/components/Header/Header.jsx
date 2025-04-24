4d8f6741270e21cb69305ea1414dcf17052e8e68
Jeev Pangli <jspanglez@gmail.com>
2025-04-24 17:11:09 +0100

    Add the SearchBar component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import styles from './Header.module.css'
+import SearchBar from '../SearchBar/SearchBar'
 
 function Header() {
   return (
     <header className={styles.header}>
-      <h1><img src='./public/reddit.svg'/> Reddit <b>Lite</b></h1>
+      <h1>
+        <img src='./public/reddit.svg'/> Reddit <b>Lite</b>
+      </h1>
+      <SearchBar />
     </header>
   )
 }
