10ac86d4934c360eac99ced9423367ced509ce63
Jeev Pangli <jspanglez@gmail.com>
2025-05-04 16:19:32 +0100

    Add subreddit to the reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import voteReducer from './voteSlice'
+import subredditReducer from './subreddditSlice'
 
 export const store = configureStore({
   reducer: {
     vote: voteReducer,
+    subreddit: subredditReducer
   },
 })
