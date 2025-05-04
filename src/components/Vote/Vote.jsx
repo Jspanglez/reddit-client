16209572c5e72760b3e22cf394fa36e384ec8d99
Jeev Pangli <jspanglez@gmail.com>
2025-05-04 16:20:58 +0100

    Utilise new icons and rework logic

--- src/components/Vote/Vote.jsx
+++ src/components/Vote/Vote.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { upvote, downvote } from '../../store/voteSlice'
 import styles from './Vote.module.css'
+import { TbArrowBigUp } from "react-icons/tb"
+import { TbArrowBigUpFilled } from "react-icons/tb"
+import { TbArrowBigDown } from "react-icons/tb"
+import { TbArrowBigDownFilled } from "react-icons/tb"
 
 function Vote() {
 
@@ -11,26 +15,31 @@ function Vote() {
   const dispatch = useDispatch()
 
   return (
-    <>
+    <div>
       <button 
-      className={upClicked ? styles.upVote : ''}
       onClick={() => dispatch(upvote()) }>
-        ⬆
+        {upClicked ? (
+          <TbArrowBigUpFilled className={styles.upVote} />
+        ) : (
+          <TbArrowBigUp />
+        )}
       </button>
       <p
-      className={
-        upClicked ? styles.upVoteNum : 
-        downClicked ? styles.downVoteNum : 
-        ''
-      }>
+        className={`${styles.defaultVoteNum} ${
+          upClicked ? styles.upVoteNum : downClicked ? styles.downVoteNum : ''
+        }`}
+      >
         {vote}
       </p>
       <button 
-      className={downClicked ? styles.downVote : ''} 
       onClick={() => dispatch(downvote()) }>
-        ⬇
+        {downClicked ? (
+          <TbArrowBigDownFilled className={styles.downVote} />
+        ) : (
+          <TbArrowBigDown />
+        )}
       </button>
-    </>
+    </div>
   )
 }
 
