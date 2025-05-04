c2b3ae2c2aa3f5b975157e383ec24f876c191c6c
Jeev Pangli <jspanglez@gmail.com>
2025-05-04 16:29:41 +0100

    Display the name of the current subreddit using redux

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import styles from './Header.module.css'
 import SearchBar from '../SearchBar/SearchBar'
+import { useSelector } from 'react-redux'
 
 function Header() {
+  const currentSubreddit = useSelector((state) => state.subreddit.current)
+
   return (
     <header className={styles.header}>
       <h1>
         <img src='./public/reddit.svg'/> Reddit <b>Lite</b>
       </h1>
       <SearchBar />
+      <h1 className={styles.currentSub}>
+        {currentSubreddit ? (
+          <>
+            Best of <span className={styles.subName}>{currentSubreddit}</span>
+          </>
+        ) : (
+          'Select a Subreddit'
+        )}
+      </h1>
     </header>
   )
 }
