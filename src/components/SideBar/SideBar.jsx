07fec858defcda34ce8670727aefe3e5c41bdae6
Jeev Pangli <jspanglez@gmail.com>
2025-05-15 16:55:53 +0100

    Refactor SideBar component to fetch subreddits from the store and handle loading and error states

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './SideBar.module.css'
 import { useDispatch, useSelector } from 'react-redux'
-import { clicked } from '../../store/subreddditSlice'
-
-const subs = []
-
-let i = 1
-let subName = 'r/Subreddit '
-
-while (i <= 10) {
-  let newSub = { key: i, name: subName + i }
-  subs.push(newSub)
-  i++
-}
+import { clicked, fetchSubreddits } from '../../store/subreddditSlice'
 
 function SideBar() {
   const dispatch = useDispatch();
-  const currentSubreddit = useSelector((state) => state.subreddit.current)
+  const { subreddits, current, loading, error } = useSelector((state) => state.subreddit)
+
+  useEffect(() => {
+    dispatch(fetchSubreddits())
+  }, [dispatch])
+
+  if (loading) {
+    return <aside className={styles.subContainer}>Loading...</aside>
+  }
+
+  if (error) {
+    return <aside className={styles.subContainer}>{error}</aside>
+  }
 
   return (
     <aside className={styles.subContainer}>
       <h2>Subreddits</h2>
-      {subs.map(x => (
+      {subreddits.map(x => (
         <button 
         key={x.key} 
-        className={currentSubreddit === x.name ? styles.activeSub : styles.subreddit}
+        className={current === x.name ? styles.activeSub : styles.subreddit}
         onClick={() => dispatch(clicked(x.name))}>
           {x.name}
         </button>
