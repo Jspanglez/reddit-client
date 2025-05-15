fc8d1da528bef87cfc9f43a002ebfbd6a4bc3ef7
Jeev Pangli <jspanglez@gmail.com>
2025-05-15 16:56:39 +0100

    Refactor SearchBar component to manage local state for search term and handle input changes

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './SearchBar.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
-function SearchBar() {
+function SearchBar({ onSearch }) {
+
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
+  const handleSearch = () => {
+    onSearch(searchTerm)
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch()
+    }
+  }
+
   return (
-    <span className={styles.span}>
+    <div className={styles.searchContainer}>
       <input 
-      type="text" 
-      className={styles.input}
-      placeholder='Search'/>
-      <button className={styles.button}>
+        type="text" 
+        className={styles.input}
+        placeholder='Search'
+        value={searchTerm}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+      />
+      <button className={styles.button} onClick={handleSearch}>
         <FontAwesomeIcon icon={faMagnifyingGlass} flip="horizontal" className={styles.icon} />
       </button>
-    </span>
+    </div>
   )
 }
 
